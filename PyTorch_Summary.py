fadc6cb89219977bb13ec4a0124d4465e7d864a5
bestfocus <51957682+bestfocus@users.noreply.github.com>
2020-09-07 02:10:48 -0400

    Create PyTorch_Summary.py

---
+++ PyTorch_Summary.py
@@ -0,0 +1,5 @@
+# The structure of training a classification model 
+# prepare train and test data: features and label (such as images or texts)
+# specify the structure of a neural network model
+# train a model
+# performance metrics
