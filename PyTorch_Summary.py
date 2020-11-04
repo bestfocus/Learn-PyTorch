8cfb2db2b82dbcc6b9205559b5009c65e2690ee0
bestfocus <51957682+bestfocus@users.noreply.github.com>
2020-11-04 13:14:17 -0500

    Update PyTorch_Summary.py

--- PyTorch_Summary.py
+++ PyTorch_Summary.py
@@ -8,3 +8,17 @@
 
 import torch
 from torchvision import datasets, transforms
+
+# tensor is a main element to be used for calculation in torch
+# A torch.Tensor is a multi-dimensional matrix containing elements of a single data type.
+# https://pytorch.org/docs/stable/tensors.html
+ta = torch.from_numpy(np.array([[1,2,3],[7,8,9]]))  
+index = torch.tensor([[1,2,1],[0,1,0]])
+ta.gather(1, index) # get some items from tensor matrix
+ta.unsqueeze(1) # add an additional dimension
+
+# use tensor to calculate gradients
+tx = torch.randn(10, requires_grad=True)
+y = tx.mean() # y is a scalor
+y.backward() # calculate gradients
+tx.grad # gradients dy/d(tx) 
